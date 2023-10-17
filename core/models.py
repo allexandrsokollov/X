9c38a83645e5bc71290b7bc93f6821444ad6c521
Alex Sokolov <allexandersokollov@gmail.com>
2023-10-17 14:56:27 +0300

    core.migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=False)
+    updated_at = models.DateTimeField(auto_now=True, db_index=False)
+
+
+class User(AbstractUser, BaseModel):
+    username = models.CharField(max_length=128, unique=True, db_index=True)
